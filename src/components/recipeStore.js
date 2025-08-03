2d6e6862f427d70e047b90e8cb2aa3f664fdf047
Henry Masereti <maseretihenry@gmail.com>
2025-08-03 20:24:07 +0300

    Fix recipeStore.js with searchTerm and setSearchTerm for checker

--- src/components/recipeStore.js
+++ src/components/recipeStore.js
@@ -1,13 +1,13 @@
 import create from 'zustand';
 
-export const useRecipeStore = create(set => ({
+export const useRecipeStore = create((set) => ({
   recipes: [],
   searchTerm: '',
   filteredRecipes: [],
   setSearchTerm: (term) => set({ searchTerm: term }),
   filterRecipes: () =>
-    set(state => ({
-      filteredRecipes: state.recipes.filter(recipe =>
+    set((state) => ({
+      filteredRecipes: state.recipes.filter((recipe) =>
         recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
       )
     }))
