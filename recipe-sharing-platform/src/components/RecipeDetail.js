7177fb23137622cd9eb6be6cd1976f031dff7871
Henry Masereti <maseretihenry@gmail.com>
2025-08-20 11:19:05 +0300

    Add Recipe Detail Page with routing and Tailwind styling

---
+++ recipe-sharing-platform/src/components/RecipeDetail.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import data from "../data.json";
+
+function RecipeDetail() {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    const foundRecipe = data.recipes.find((r) => r.id === parseInt(id));
+    setRecipe(foundRecipe);
+  }, [id]);
+
